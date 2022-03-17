98e4d7bb8c2468d2d605161cdc43daa6a5c2c274
FelesFurva <kitten.solar@gmail.com>
2022-03-17 13:36:09 +0200

    Add project files.

---
+++ Number guessing game/Program.cs
@@ -0,0 +1,24 @@
+﻿int?[] winingNumbers = { 2, 7, 9 };
+int guessedRight = 0;
+Console.WriteLine("Guess three numbers and you win!");
+Console.WriteLine("Please write the number!");
+
+
+while (guessedRight != 3)
+{
+    int.TryParse(Console.ReadLine(), out int userNumber);
+
+    if (!winingNumbers.Contains(userNumber))
+    {
+        Console.WriteLine("Nope! You guessed wrong!");
+        Console.WriteLine("One more time!");
+    }
+    else
+    {
+        Console.WriteLine("You guessed right!");
+        int guessedNum = Array.IndexOf(winingNumbers, userNumber);
+        winingNumbers[guessedNum] = null;      
+        guessedRight++;
+    }
+} 
+Console.WriteLine("Congratulation! You won!");
